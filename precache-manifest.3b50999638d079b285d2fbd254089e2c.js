462cec1c9da6c306706a43551fee4e3ce9cfa2f1
anu-portfolio <Anu@Anu-MacBook-Pro.local>
2021-04-22 15:23:13 -0700

    first commit

---
+++ precache-manifest.3b50999638d079b285d2fbd254089e2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cdd4ce2c819b832d31f76805ba5a16",
+    "url": "./index.html"
+  },
+  {
+    "revision": "816818244613c2425049",
+    "url": "./static/css/main.38100193.chunk.css"
+  },
+  {
+    "revision": "128e61bfc2573a29fe8a",
+    "url": "./static/js/2.8e1a1cc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8e1a1cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816818244613c2425049",
+    "url": "./static/js/main.ffeee690.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  },
+  {
+    "revision": "61b683e46315e39d73f61b1ff8ccfb73",
+    "url": "./static/media/background-large.61b683e4.png"
+  },
+  {
+    "revision": "8fd876c335cb957dc75a0d279f4c84ae",
+    "url": "./static/media/background-medium.8fd876c3.png"
+  },
+  {
+    "revision": "783442e7983c2e4d250536eebdec6809",
+    "url": "./static/media/background-small.783442e7.png"
+  }
+]);
